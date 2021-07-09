564f87fbabf0ae85692c79a8d3d63048e4c300ae
Quentin Guillou <quentinguillou91@gmail.com>
2021-07-09 22:27:21 +0200

    gestion des routes angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { MarquesListComponent } from './components/marques-list/marques-list.component';
+import { MarqueDetailsComponent } from './components/marque-details/marque-details.component';
+import { AddMarqueComponent } from './components/add-marque/add-marque.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'marques', pathMatch: 'full' },
+  { path: 'marques', component: MarquesListComponent },
+  { path: 'marques/:id', component: MarqueDetailsComponent },
+  { path: 'ajouter', component: AddMarqueComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
