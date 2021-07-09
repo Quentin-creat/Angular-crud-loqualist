2f649e0eeef95978b70bd901fc487f7bb25c77d5
Quentin Guillou <quentinguillou91@gmail.com>
2021-07-09 22:30:54 +0200

    envoie des requetes à l'API

--- src/app/services/marque.service.ts
+++ src/app/services/marque.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Marque } from '../models/marque.model';
+
+const baseUrl = 'http://localhost:8080/api/marques';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarqueService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Marque[]> {
+    return this.http.get<Marque[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Marque> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
 }
